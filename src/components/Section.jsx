8c5f784e048d21ad734d1b2fe689bb0c8bdefaa0
jinoc <choiae369@gmail.com>
2023-06-22 11:46:52 +0900

    Feat: todo상태 변경

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -2,11 +2,20 @@ import React from "react";
 import uuid from "react-uuid";
 import Todo from "./Todo";
 
-function Section({ todos, isDone }) {
+function Section({ todos, isDone, setTodos }) {
+  const chageIsDone = (id) => {
+    const newTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.isDone = !todo.isDone;
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  };
   return (
     <section>
-      <h2>{isDone ? 'Working' : 'Done'}</h2>
-      <Todo todos={todos} isDone={isDone} />
+      <h2>{isDone ? "Done" : "Working"}</h2>
+      <Todo todos={todos} isDone={isDone} chageIsDone={chageIsDone} />
     </section>
   );
 }
